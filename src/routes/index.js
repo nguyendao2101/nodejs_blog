357bd42a3e9fef2005c89d10a4799cfab4466953
nguyen dao <nguyendao21012002@gmail.com>
2025-04-21 16:02:05 +0700

    course detail page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const newsRouter = require('./news'); // địa chỉ file news.js trong thư mục routes
+const coursesRouter = require('./courses'); // địa chỉ file news.js trong thư mục routes
 const siteRouter = require('./site'); // địa chỉ file site.js trong thư mục routes
 
 function route(app) {
     app.use('/', siteRouter);
     app.use('/news', newsRouter);
+    app.use('/courses', coursesRouter);
     app.use('/search', siteRouter);
 }
 
